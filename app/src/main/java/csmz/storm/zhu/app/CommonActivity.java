f191f56d8797045ea3b294f31fe3ae92c3096e6b
stormzhu <vernon_zlb@163.com>
2017-08-02 10:49:33 +0800

    添加ComminActivity

---
+++ app/src/main/java/csmz/storm/zhu/app/CommonActivity.java
@@ -0,0 +1,15 @@
+package csmz.storm.zhu.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by uniface on 2017/8/2.
+ */
+
+public class CommonActivity extends AppCompatActivity {
+
+
+
+
+
+}
