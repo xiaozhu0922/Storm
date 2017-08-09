a1a717a2bba00500c723bf7af39ddc4df71c55ad
zhuliubin5201314@qq.com <zhu19950922>
2017-08-10 00:59:34 +0800

    测试提交

---
+++ app/src/main/java/csmz/storm/zhu/diary/db/kk.java
@@ -0,0 +1,19 @@
+package csmz.storm.zhu.diary.db;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+import csmz.storm.zhu.app.CommonActivity;
+
+/**
+ * Created by Administrator on 2017/8/10.
+ */
+
+public class kk extends CommonActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
